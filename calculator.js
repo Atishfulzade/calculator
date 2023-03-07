01ff297dd76cbb2afae302ee6a14d8486c345c9f
Atishfulzade <118718295+Atishfulzade@users.noreply.github.com>
2023-03-07 17:27:32 +0530

    calculator

---
+++ calculator.js
@@ -0,0 +1,29 @@
+
+  let display = document.querySelector(".display");
+  let buttons = document.querySelectorAll(".button");
+  let del = document.querySelector(".delete");
+  let allclear = document.querySelector(".all-clear");
+  let equal = document.querySelector(".equal");
+  let temp=""
+
+  
+  let myImg
+function myFunction() {
+  var x = document.getElementsByClassName("myImg").src;
+  document.getElementById("demo").innerHTML = x;
+}
+ const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const lightModeStylesheet = document.querySelector('link[href="calculator.css"]');
+const darkModeStylesheet = document.querySelector('link[href="dark.css"]');
+
+darkModeToggle.addEventListener('click', () => {
+  if (darkModeStylesheet.disabled) {
+    darkModeStylesheet.disabled = false;
+    lightModeStylesheet.disabled = true;
+    darkModeToggle.getElementsByClassName(myImg);
+  } else {
+    lightModeStylesheet.disabled = false;
+    darkModeStylesheet.disabled = true;
+    darkModeToggle.getElementsByClassName(myImg);
+  }
+});
